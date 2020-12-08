f1c1389806f5968d98bac5de07946d3ec112f7fe
Julia Foresti <juliajforesti@gmail.com>
2020-12-08 14:58:01 -0300

    user model done, user routes started

--- app.js
+++ app.js
@@ -13,4 +13,8 @@ app.use(cors({ origin: "http://localhost:3000" }));
 const bookRouter = require("./routes/book.routes");
 app.use("/", bookRouter);
 
+const userRouter = require('./routes/user.routes')
+app.use('/', userRouter);
+
+
 app.listen(PORT, () => console.log(`Server up and running at port ${PORT}`));
