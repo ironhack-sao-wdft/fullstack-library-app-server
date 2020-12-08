5ac25a9a820ecbd5ec52face5ce03998bc0f4e10
rschpdr <pedro.resch@gmail.com>
2020-12-08 17:46:30 -0300

    started auth using passport

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
 const express = require("express");
 const cors = require("cors");
 const db = require("./config/db.config");
-db();
+const passportConfig = require("./config/passport.config");
 
 const PORT = 1234;
 
 const app = express();
 
+db();
+passportConfig(app);
+
 app.use(express.json());
 app.use(cors({ origin: "http://localhost:3000" }));
 
 const bookRouter = require("./routes/book.routes");
-app.use("/", bookRouter);
+app.use("/api", bookRouter);
 
-const userRouter = require('./routes/user.routes')
-app.use('/', userRouter);
-
+const userRouter = require("./routes/user.routes");
+app.use("/api", userRouter);
 
 app.listen(PORT, () => console.log(`Server up and running at port ${PORT}`));
