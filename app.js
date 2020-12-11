c10bc90ed72acb78dde6e43d0c95e6393a5c2a44
rschpdr <pedro.resch@gmail.com>
2020-12-11 10:44:49 -0300

    testing urlencoded

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ const app = express();
 const publicPath = path.join(__dirname, "public");
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 app.use(express.static(publicPath));
 
