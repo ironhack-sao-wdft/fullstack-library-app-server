f7184e996fa38ae7f1ad04c48f3c08a4c8c94310
rschpdr <pedro.resch@gmail.com>
2020-12-11 10:42:38 -0300

    second way of deploying

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 const db = require("./config/db.config");
 const passportConfig = require("./config/passport.config");
 
 const app = express();
 
+const publicPath = path.join(__dirname, "public");
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(cors());
+app.use(express.static(publicPath));
 
 db();
 passportConfig(app);
+
+app.get("*", (req, res, next) => {
+  const hostUrl = req.originalUrl;
+  if (!hostUrl.includes("/api")) {
+    console.log(hostUrl);
+    return res.sendFile(path.join(publicPath, "index.html"));
+  }
+  return next();
+});
 
 const bookRouter = require("./routes/book.routes");
 app.use("/api", bookRouter);
